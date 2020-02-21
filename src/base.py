844a5815a9ca7118220440fd08251c75842bb7cf
TheRincon <rinconrex@gmail.com>
2020-02-21 08:49:54 +0100

    refactor base and tools

--- src/base.py
+++ src/base.py
@@ -1,6 +1,8 @@
 import unittest
 from selenium import webdriver
 from config import Config
+import tools as tl
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import selenium_setup as selenium_setter
 
@@ -11,13 +13,14 @@ class Base(unittest.TestCase):
     def setUp(self):
         self.driver = self.DRIVER
         self.driver.maximize_window()
-        self.driver.get("http://www.google.com/")
-
-    def test_search_box(self):
-        self.assertTrue(self.is_element_present(By.NAME, "q"))
 
     def tearDown(self):
         self.driver.quit()
+
+class SampleTest(Base):
+    def test_search_box(self):
+        self.driver.get("http://www.google.com/")
+        self.assertTrue(tl.is_element_present(self.driver, By.NAME, "q"))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
